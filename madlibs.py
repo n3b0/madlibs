60422d881afc720d797490f9c9972b6a58d66866
n3b0 <86699287+n3b0@users.noreply.github.com>
2021-11-09 11:32:15 +0300

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+
+adj = input("Adjective: ")
+verb1 = input("Verb ")
+verb2 = input("Verb ")
+famous_person = input("Famous Person ")
+
+madlib = f"Computer progrmamming is so {adj}! It makes me so excited because \
+I love to {verb1}. Stay hydrate and {verb2} like you are {famous_person}!"
+
+print(madlib)
